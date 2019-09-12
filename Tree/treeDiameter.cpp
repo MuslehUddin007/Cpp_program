faf9a5de44ddf0d0b658bef455c68977b0650beb
MuslehUddin007 <muslehuddin0010@gmail.com>
2019-09-12 14:26:37 +0600

    commit -m "added"

--- Tree/treeDiameter.cpp
+++ Tree/treeDiameter.cpp
@@ -22,20 +22,24 @@ int height(struct Node * node){
     return 1 + max(height(node->left),height(node->right));
 }
 
-/*int diameter(struct Node *tree){
+int diameter(struct Node *tree){
     if(tree == NULL)
         return 0;
 
     int lheight = height(tree->left);
     int rheight = height(tree->right);
+    //cout << "Lheight : " << lheight << endl;
+    //cout << "Rheight : " << rheight << endl;
 
     int ldiameter = diameter(tree->left);
     int rdiameter = diameter(tree->right);
+    //cout << "Ldiameter : " << ldiameter << endl;
+    //cout << "Rdiameter : " << rdiameter << endl;
 
     return max(lheight + rheight + 1, max(ldiameter,rdiameter));
-}*/
+}
 
-int diameter1(struct Node *tree){
+/*int diameter1(struct Node *tree){
     if(tree == NULL)
         return 0;
 
@@ -44,7 +48,7 @@ int diameter1(struct Node *tree){
     int option3 = diameter1(tree->right);
 
     return max(option1 + 1,max(option2,option3));
-}
+}*/
 
 int diameterOpt(struct Node *root, int *height){
     int lh=0,rh=0;
@@ -55,6 +59,10 @@ int diameterOpt(struct Node *root, int *height){
     }
     ldiameter = diameterOpt(root->left,&lh);
     rdiameter = diameterOpt(root->right,&rh);
+    cout << "Lheight : " << lh << endl;
+    cout << "Rheight : " << rh << endl;
+    cout << "Ldiameter : " << ldiameter << endl;
+    cout << "Rdiameter : " << rdiameter << endl;
     *height = max(lh,rh)+1;
     return max(lh+rh+1,max(ldiameter,rdiameter));
 }
@@ -67,8 +75,13 @@ int main()
     root->right = new Node(3);
     root->left->left = new Node(4);
     root->left->right = new Node(5);
+    root->left->left->left = new Node(6);
+    root->left->left->right = new Node(7);
+    root->left->left->right->left = new Node(8);
+    root->left->left->right->right = new Node(9);
+    root->left->left->left->left = new Node(10);
 
-    cout << "Diameter of given binary tree is : " << diameter1(root) << endl;
+    cout << "Diameter of given binary tree is : " << diameter(root) << endl;
     cout << "Diameter of given binary tree is : " << diameterOpt(root,&height) << endl;
 
     return 0;
